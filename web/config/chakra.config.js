7933127cd55e12be88d526e9e4714f8bebd642fb
Sunfee <sdk6@njit.edu>
2023-10-17 20:29:04 -0400

    first commit

---
+++ web/config/chakra.config.js
@@ -0,0 +1,4 @@
+// This object will be used to override Chakra-UI theme defaults.
+// See https://chakra-ui.com/docs/styled-system/theming/theme for theming options
+const theme = {}
+export default theme
