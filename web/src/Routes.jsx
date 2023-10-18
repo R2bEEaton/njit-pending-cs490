3e82a25bfb6ad5367b1a7e8b38c2045cf66d63e8
Jeremy Switlyk <jeremy.switlyk@protonmail.ch>
2023-10-18 16:28:19 -0400

    Setup dbAuth

--- web/src/Routes.jsx
+++ web/src/Routes.jsx
@@ -9,9 +9,11 @@
 
 import { Router, Route } from '@redwoodjs/router'
 
+import { useAuth } from './auth'
+
 const Routes = () => {
   return (
-    <Router>
+    <Router useAuth={useAuth}>
       <Route path="/login" page={LoginPage} name="login" />
       <Route notfound page={NotFoundPage} />
     </Router>
