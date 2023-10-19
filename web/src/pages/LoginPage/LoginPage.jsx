96c126e94e1043334a8246975edb045f0bbd4854
Ryan Eaton <rle22@njit.edu>
2023-10-18 22:24:09 -0400

    implement google oauth, which took a while

--- web/src/pages/LoginPage/LoginPage.jsx
+++ web/src/pages/LoginPage/LoginPage.jsx
@@ -1,5 +1,6 @@
 import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
+import { useAuth } from "src/auth";
 
 const LoginPage = () => {
   return (
@@ -14,6 +15,11 @@ const LoginPage = () => {
         My default route is named <code>login</code>, link to me with `
         <Link to={routes.login()}>Login</Link>`
       </p>
+
+      <a href={`https://accounts.google.com/o/oauth2/v2/auth?scope=https%3A//www.googleapis.com/auth/calendar.events.readonly%20https%3A//www.googleapis.com/auth/userinfo.email%20https%3A//www.googleapis.com/auth/userinfo.profile&access_type=offline&include_granted_scopes=true&response_type=code&redirect_uri=${process.env.GOOGLE_OAUTH_REDIRECT_URI}&client_id=${process.env.GOOGLE_OAUTH_CLIENT_ID}`}
+         className="mx-auto block w-48 rounded bg-gray-800 px-4 py-2 text-center text-xs font-semibold uppercase tracking-wide text-white">
+        Login with Google
+      </a>
     </>
   )
 }
