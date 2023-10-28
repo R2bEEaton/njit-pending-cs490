02f9582763b6c3040e0a83df2a27f30fa87b2acc
Ryan Eaton <rle22@njit.edu>
2023-10-28 07:40:57 -0400

    Main layout scaffold

--- web/src/layouts/MainLayout/MainLayout.jsx
+++ web/src/layouts/MainLayout/MainLayout.jsx
@@ -1,5 +1,14 @@
+import {ChakraProvider, Stack} from "@chakra-ui/react";
+import theme from "src/pages/LoginPage/theme";
+import {MetaTags} from "@redwoodjs/web";
+
 const MainLayout = ({ children }) => {
-  return <>{children}</>
+  return (
+    <ChakraProvider theme={theme}>
+      <h1>test</h1>
+      <>{children}</>
+    </ChakraProvider>
+  )
 }
 
 export default MainLayout
