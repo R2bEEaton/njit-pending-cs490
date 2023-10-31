b31d68c6221c7c5f690d9205927c5f7e86d3d262
Brendan Clayton <bren530.clayton@gmail.com>
2023-10-31 15:52:27 -0400

    Updated Settings test

--- web/src/pages/SettingsPage/SettingsPage.test.jsx
+++ web/src/pages/SettingsPage/SettingsPage.test.jsx
@@ -1,14 +1,59 @@
-import { render } from '@redwoodjs/testing/web'
+import { render, screen, waitFor } from '@redwoodjs/testing/web';
+import userEvent from '@testing-library/user-event';
+import SettingsPage from './SettingsPage';
 
-import SettingsPage from './SettingsPage'
+jest.mock('src/auth', () => ({
+  useAuth: () => ({
+    currentUser: {
+      id: 1,
+      firstName: 'John',
+      lastName: 'Doe',
+      pomodoro: 25,
+      shortBreak: 5,
+      longBreak: 15,
+    },
+    reauthenticate: jest.fn(),
+  }),
+}));
 
-//   Improve this test with help from the Redwood Testing Doc:
-//   https://redwoodjs.com/docs/testing#testing-pages-layouts
 
 describe('SettingsPage', () => {
-  it('renders successfully', () => {
-    expect(() => {
-      render(<SettingsPage />)
-    }).not.toThrow()
-  })
-})
+  it('calls onSubmit when the form is filled', async () => {
+    const onSubmit = jest.fn(); // Mock the onSubmit function
+    const fname = 'Joe'
+    const lname = 'Blow'
+    const pomodoro = '1'
+    const sbreak = '2'
+    const lbreak = '3'
+    render(<SettingsPage />);
+
+    const firstNameInput = screen.getByLabelText('First Name');
+    const lastNameInput = screen.getByLabelText('Last Name');
+    const pomodoroInput = screen.getByLabelText('Pomodoro');
+    const shortBreakInput = screen.getByLabelText('Short Break');
+    const longBreakInput = screen.getByLabelText('Long Break');
+    const submitButton = screen.getByText('Save');
+
+    await waitFor(() => userEvent.type(firstNameInput, fname))
+    await waitFor(() => userEvent.type(lastNameInput, lname))
+    await waitFor(() => userEvent.type(pomodoroInput, pomodoro))
+    await waitFor(() => userEvent.type(shortBreakInput, sbreak))
+    await waitFor(() => userEvent.type(longBreakInput, lbreak))
+    await waitFor(() => userEvent.click(submitButton))
+
+    expect(onSubmit).toHaveBeenCalled()
+  });
+
+  it('does not submit when required fields are empty', () => {
+    const onSubmit = jest.fn();
+
+    render(<SettingsPage />);
+
+    const submitButton = screen.getByText('Save');
+
+    userEvent.click(submitButton);
+
+    // Ensure that the onSubmit function is not called
+    expect(onSubmit).not.toHaveBeenCalled();
+  });
+});
