88e40123cda561105c01c73259b1407725ead430
Jeremy Switlyk <jeremy.switlyk@protonmail.ch>
2023-11-02 19:13:56 -0400

    Fix Chevron Color

--- web/src/components/DatePicker/DatePicker.jsx
+++ web/src/components/DatePicker/DatePicker.jsx
@@ -18,6 +18,8 @@ import {
   MenuGroup,
   MenuOptionGroup,
   MenuDivider,
+  Wrap,
+  WrapItem,
 } from '@chakra-ui/react'
 
 import { useForm } from '@redwoodjs/forms'
@@ -32,59 +34,74 @@ let month = new Date().toLocaleString('default', { month: 'long' })
 
 const DatePicker = () => {
   return (
-    <Box bg={'#6284FF26'} borderRadius={'md'} color={'white'}>
-      <Menu size={'md'}>
-        {({ isOpen }) => (
-          <>
-            <MenuButton
-              isActive={isOpen}
-              as={Button}
-              rightIcon={isOpen ? <ChevronUpIcon /> : <ChevronDownIcon />}
-            >
-              {month}
-            </MenuButton>
-            <MenuList>
-              <MenuItem color={'black'} onClick={() => (month = 'January')}>
-                January
-              </MenuItem>
-              <MenuItem color={'black'} onClick={() => (month = 'February')}>
-                February
-              </MenuItem>
-              <MenuItem color={'black'} onClick={() => (month = 'March')}>
-                March
-              </MenuItem>
-              <MenuItem color={'black'} onClick={() => (month = 'April')}>
-                April
-              </MenuItem>
-              <MenuItem color={'black'} onClick={() => (month = 'May')}>
-                May
-              </MenuItem>
-              <MenuItem color={'black'} onClick={() => (month = 'June')}>
-                June
-              </MenuItem>
-              <MenuItem color={'black'} onClick={() => (month = 'July')}>
-                July
-              </MenuItem>
-              <MenuItem color={'black'} onClick={() => (month = 'August')}>
-                August
-              </MenuItem>
-              <MenuItem color={'black'} onClick={() => (month = 'September')}>
-                September
-              </MenuItem>
-              <MenuItem color={'black'} onClick={() => (month = 'October')}>
-                October
-              </MenuItem>
-              <MenuItem color={'black'} onClick={() => (month = 'November')}>
-                November
-              </MenuItem>
-              <MenuItem color={'black'} onClick={() => (month = 'December')}>
-                December
-              </MenuItem>
-            </MenuList>
-          </>
-        )}
-      </Menu>
-    </Box>
+    <Wrap
+      spacing={'30px'}
+      h={'60px'}
+      bg={'#6284FF26'}
+      borderRadius={'md'}
+      justify={'center'}
+      align={'center'}
+    >
+      <WrapItem>
+        <Menu size={'md'}>
+          {({ isOpen }) => (
+            <>
+              <MenuButton
+                isActive={isOpen}
+                as={Button}
+                rightIcon={
+                  isOpen ? (
+                    <ChevronUpIcon color={'#6284FF'} />
+                  ) : (
+                    <ChevronDownIcon color={'#6284FF'} />
+                  )
+                }
+              >
+                {month}
+              </MenuButton>
+              <MenuList>
+                <MenuItem color={'black'} onClick={() => (month = 'January')}>
+                  January
+                </MenuItem>
+                <MenuItem color={'black'} onClick={() => (month = 'February')}>
+                  February
+                </MenuItem>
+                <MenuItem color={'black'} onClick={() => (month = 'March')}>
+                  March
+                </MenuItem>
+                <MenuItem color={'black'} onClick={() => (month = 'April')}>
+                  April
+                </MenuItem>
+                <MenuItem color={'black'} onClick={() => (month = 'May')}>
+                  May
+                </MenuItem>
+                <MenuItem color={'black'} onClick={() => (month = 'June')}>
+                  June
+                </MenuItem>
+                <MenuItem color={'black'} onClick={() => (month = 'July')}>
+                  July
+                </MenuItem>
+                <MenuItem color={'black'} onClick={() => (month = 'August')}>
+                  August
+                </MenuItem>
+                <MenuItem color={'black'} onClick={() => (month = 'September')}>
+                  September
+                </MenuItem>
+                <MenuItem color={'black'} onClick={() => (month = 'October')}>
+                  October
+                </MenuItem>
+                <MenuItem color={'black'} onClick={() => (month = 'November')}>
+                  November
+                </MenuItem>
+                <MenuItem color={'black'} onClick={() => (month = 'December')}>
+                  December
+                </MenuItem>
+              </MenuList>
+            </>
+          )}
+        </Menu>
+      </WrapItem>
+    </Wrap>
   )
 }
 
