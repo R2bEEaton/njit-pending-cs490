d989a93f943d119b96ebd48966cfb2f722126e3a
Jeremy Switlyk <jeremy.switlyk@protonmail.ch>
2023-11-06 16:56:43 -0500

    Add Styling

--- web/src/components/DatePicker/DatePicker.jsx
+++ web/src/components/DatePicker/DatePicker.jsx
@@ -20,6 +20,8 @@ import {
   MenuDivider,
   Wrap,
   WrapItem,
+  Icon,
+  createIcon,
 } from '@chakra-ui/react'
 
 import { useForm } from '@redwoodjs/forms'
@@ -31,6 +33,32 @@ import { Toaster } from '@redwoodjs/web/toast'
 import { useAuth } from 'src/auth'
 
 let month = new Date().toLocaleString('default', { month: 'long' })
+
+const CircleChevronDownIcon = (props) => (
+  <Icon
+    with={'20'}
+    height={'20'}
+    viewbox={'0 0 20 20'}
+    fill={'none'}
+    {...props}
+  >
+    <path
+      d="M18.3334 10C18.3334 5.39767 14.6025 1.66671 10.0001 1.66671C5.39771 1.66671 1.66675 5.39767 1.66675 10C1.66675 14.6024 5.39771 18.3334 10.0001 18.3334C14.6025 18.3334 18.3334 14.6024 18.3334 10Z"
+      stroke="#6284FF"
+      strokeWidth="1.5"
+      strokeMiterlimit="10"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M12.9417 8.94997L10 11.8833L7.05835 8.94997"
+      stroke="#6284FF"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </Icon>
+)
 
 const DatePicker = () => {
   return (
@@ -54,7 +82,7 @@ const DatePicker = () => {
                   isOpen ? (
                     <ChevronUpIcon color={'#6284FF'} />
                   ) : (
-                    <ChevronDownIcon color={'#6284FF'} />
+                    <CircleChevronDownIcon />
                   )
                 }
               >
