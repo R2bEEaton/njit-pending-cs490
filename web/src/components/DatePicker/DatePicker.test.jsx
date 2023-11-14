14688d4fa742d6189b439480ffef905066117b43
Jeremy Switlyk <jeremy.switlyk@protonmail.ch>
2023-11-14 14:34:50 -0500

    Add Mock for setDateProp

--- web/src/components/DatePicker/DatePicker.test.jsx
+++ web/src/components/DatePicker/DatePicker.test.jsx
@@ -5,10 +5,12 @@ import DatePicker from './DatePicker'
 //   Improve this test with help from the Redwood Testing Doc:
 //    https://redwoodjs.com/docs/testing#testing-components
 
+const mockSetDateProp = jest.fn()
+
 describe('DatePicker', () => {
   it('renders successfully', () => {
     expect(() => {
-      render(<DatePicker />)
+      render(<DatePicker setDateProp={mockSetDateProp} />)
     }).not.toThrow()
   })
 })
