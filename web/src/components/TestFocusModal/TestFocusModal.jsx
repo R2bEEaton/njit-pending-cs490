3e4d6dd2c1e9d8c77d26494f162f9d5cee17fbc3
Jeremy Switlyk <jeremy.switlyk@protonmail.ch>
2023-11-22 14:13:07 -0500

    Basic Modal Structure

--- web/src/components/TestFocusModal/TestFocusModal.jsx
+++ web/src/components/TestFocusModal/TestFocusModal.jsx
@@ -1,11 +1,30 @@
-const TestFocusModal = () => {
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+} from '@chakra-ui/react'
+
+const TestFocusModal = ({ isOpen, onOpen, onClose }) => {
   return (
-    <div>
-      <h2>{'TestFocusModal'}</h2>
-      <p>
-        {'Find me in ./web/src/components/TestFocusModal/TestFocusModal.jsx'}
-      </p>
-    </div>
+    <>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>Modal Body</ModalBody>
+          <ModalFooter>
+            <p>Modal Footer</p>
+            <Button onClick={onClose}>Close</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   )
 }
 
