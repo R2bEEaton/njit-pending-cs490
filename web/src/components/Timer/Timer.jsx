9738963b08f6355506536172c72b81b6cbe53b60
Jeremy Switlyk <jeremy.switlyk@protonmail.ch>
2023-11-30 16:28:43 -0500

    Add Start Buttons to Timer

--- web/src/components/Timer/Timer.jsx
+++ web/src/components/Timer/Timer.jsx
@@ -6,22 +6,29 @@ import { setInterval, clearInterval } from 'worker-timers'
 const Timer = ({ numMinutes }) => {
   const [minutes, setMinutes] = useState(numMinutes)
   const [seconds, setSeconds] = useState(0)
+  const [isRunning, setIsRunning] = useState(false)
 
   useEffect(() => {
+    let timer
     if (minutes === 0 && seconds === 0) return
-    const timer = setInterval(() => {
-      if (seconds === 0) {
-        setMinutes((prevMinutes) => prevMinutes - 1)
-        setSeconds(59)
-      } else setSeconds((prevSeconds) => prevSeconds - 1)
-    }, 1000)
+    if (isRunning && (minutes > 0 || seconds > 0)) {
+      timer = setInterval(() => {
+        if (seconds === 0) {
+          setMinutes((prevMinutes) => prevMinutes - 1)
+          setSeconds(59)
+        } else setSeconds((prevSeconds) => prevSeconds - 1)
+      }, 1000)
+    }
     return () => clearInterval(timer)
-  }, [minutes, seconds])
+  }, [isRunning, minutes, seconds])
 
   return (
-    <p>
+    <div>
       {minutes}:{seconds < 10 ? '0' + seconds : seconds}
-    </p>
+      <Button onClick={() => setIsRunning(true)} disabled={isRunning}>
+        start
+      </Button>
+    </div>
   )
 }
 
