6a73d3a06fb8ea8c7e4f0fbe7c75f731778e60d2
Ryan Eaton <rle22@njit.edu>
2023-12-07 16:14:11 -0500

    Hide plan day button on the profile page

--- web/src/layouts/MainLayout/MainLayout.jsx
+++ web/src/layouts/MainLayout/MainLayout.jsx
@@ -27,10 +27,12 @@ const MainLayout = ({ children }) => {
             <Image mt={"10vh"} src={"img/pending.png"} />
             { /*
             TODO:
-            The following will need to be selectively hidden if the user has already planned their day
+            The following will also need to be selectively hidden if the user has already planned their day
             */ }
-            <Text textAlign={"center"} fontSize={20} fontFamily={"DM Sans"} fontWeight={"700"}>It's time to plan your day!</Text>
-            <Button colorScheme={"white"} variant={"outline"} w={"100%"} pt={7} pb={7} href={`/.redwood/functions/todaysCalendar?userId=${currentUser?.id}`} as={'a'}>Plan Day</Button>
+            <Box hidden={title === 'Profile'}>
+              <Text textAlign={"center"} fontSize={20} fontFamily={"DM Sans"} fontWeight={"700"}>It's time to plan your day!</Text>
+              <Button colorScheme={"white"} variant={"outline"} w={"100%"} pt={7} pb={7} href={`/.redwood/functions/todaysCalendar?userId=${currentUser?.id}`} as={'a'}>Plan Day</Button>
+            </Box>
             <Spacer />
             <Button colorScheme={"white"} variant={"outline"} type='button' onClick={logOut} mb={'5vh'}><LogoutIcon />Logout</Button>
           </Flex>
