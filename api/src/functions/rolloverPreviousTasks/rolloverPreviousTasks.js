ac8fc4b01f4491959dd48ca6563b3dc56c7f64ee
sdk6 <sdk6@njit.edu>
2023-12-17 04:17:49 -0500

    other.length >0

--- api/src/functions/rolloverPreviousTasks/rolloverPreviousTasks.js
+++ api/src/functions/rolloverPreviousTasks/rolloverPreviousTasks.js
@@ -23,30 +23,33 @@ import {db} from 'src/lib/db';
 
 export const handler = async (event, _context) => {
   logger.info(`${event.httpMethod} ${event.path}: rolloverPreviousTasks function`)
-  function substring(string) {
+  /*function substring(string) {
     var result = '';
     i = 0;
     while (string[i] != "T") result += string[i++];
     return result;
-  }
+  }*/
   function oldTasks(tasks, today) {
     let result=[];
     let len = tasks.length;
     let i = 0;
     while(i<len){
-      if (tasks[i].date<today) result+= tasks[i].id;
+      if (tasks[i].date<today){
+        if (tasks[i].taskList.Other.length > 0 )result+= tasks[i].id
+      } //result+= tasks[i].id;
       i++;
     }
     return result;
   }
   // Get the current user
   const { userId, date} = event.queryStringParameters
-  const dd= new Date("2023-12-14")
+  const dd= new Date("2023-12-15")
   const authUser = await authDecoder(userId, 'dbAuth', {event, context})
   const currentUser = await getCurrentUser(authUser)
   const allTasks = currentUser.tasks
   const olTasks = oldTasks(allTasks, dd)
-  //const vr = currentUser.tasks[0].date
+  //const vr = currentUser.tasks[2].taskList
+  //const f = vr.Other.length
 
   return {
     statusCode: 200,
@@ -59,8 +62,8 @@ export const handler = async (event, _context) => {
       dd,
       allTasks,
       olTasks,
-      //vr
-
+      //vr,
+      //f
     }),
   }
 }
